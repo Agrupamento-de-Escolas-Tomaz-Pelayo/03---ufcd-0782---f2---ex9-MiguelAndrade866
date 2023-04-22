6dddba5b4b7254dc4f302f5c7e1d8b6c6eadaec1
MiguelAndrade866 <a31366@tomazpelayo.com>
2023-04-22 11:59:03 +0100

    exercicio9

--- ASCIIArt.c
+++ ASCIIArt.c
@@ -1,3 +1,19 @@
-// Resolva neste ficheiro o seu exercício
+#include <stdio.h>
+int main()
+{
+    printf("          _______\n");
+    printf("         |.-----.|\n");
+    printf("         ||x . x||\n");
+    printf("         ||_.-._||\n");
+    printf("         `--)-(--`\n");
+    printf("        __[=== o]___\n");
+    printf("       |:::::::::::|\ \n");
+    printf("       `-=========-`()\n");
+
+    return(0);
+
+}
+
+
 
 
